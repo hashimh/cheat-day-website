7817e60c130b363dc7c91a1aa95e6eddae6c7bc9
Hashim Hussain <hashim_hussain98@hotmail.co.uk>
2019-10-19 16:25:40 +0100

    minor menu.css changes

--- webpages/index.js
+++ webpages/index.js
@@ -12,19 +12,29 @@ function navFunction() {
 // FUNCTION TO ALTER CLASSNAMES OF SIDENAV ELEMENTS FOR RESPONSIVENESS //
 document.addEventListener("DOMContentLoaded", function() {
   let links = document.getElementsByClassName("side_a");
+  console.log(links)
+
+  // Add event listener for clicking links
   for (let i = 0; i < links.length; i++) {
     links[i].addEventListener("click", function() {
-      let current = document.getElementsByClassName("active_a");
-      current[0].className = current[0].className.replace(" active_a", "");
-      this.className += " active_a";
-
-      // Scroll into view what has been clicked
       let div = document.getElementsByName(this.innerHTML)
       div[0].scrollIntoView({behavior: 'smooth'});
     });
   }
+
+  // Add event listener for scrolling to "links"
+  let divs = document.getElementsByClassName("menuitem");
+  window.addEventListener("scroll", function() {
+    let index = divs.length;
+    while (--index && window.scrollY + 100 < divs[index].offsetTop) {}
+    for (let i = 0; i < links.length; i++) {
+      links[i].classList.remove('active_a');
+      links[index].classList.add('active_a');
+    }
+  });
 });
 
+// FUNCTION TO RESET PAGE LOCATION ON REFRESH
 window.onbeforeunload = function () {
   window.scrollTo(0, 0);
 }
