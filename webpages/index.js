15d9062c792f9a0ab5ac2e23b29934e1174af2c0
Hashim Hussain <hashim_hussain98@hotmail.co.uk>
2019-10-19 14:15:47 +0100

    menu.html side nav added
    responsive-ish, fixed and active class works

--- webpages/index.js
+++ webpages/index.js
@@ -8,3 +8,19 @@ function navFunction() {
     x.className = "topnav";
   }
 }
+
+// FUNCTION TO ALTER CLASSNAMES OF SIDENAV ELEMENTS FOR RESPONSIVENESS //
+document.addEventListener("DOMContentLoaded", function() {
+  let links = document.getElementsByClassName("side_a");
+  for (let i = 0; i < links.length; i++) {
+    links[i].addEventListener("click", function() {
+      let current = document.getElementsByClassName("active_a");
+      current[0].className = current[0].className.replace(" active_a", "");
+      this.className += " active_a";
+    });
+  }
+});
+
+window.onbeforeunload = function () {
+  window.scrollTo(0, 0);
+}
