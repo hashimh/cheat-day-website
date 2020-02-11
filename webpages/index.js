d01af6a60fcbeecc5227f9706bc3761065e47d7e
Hashim Hussain <up815459@myport.ac.uk>
2020-02-11 14:11:56 +0000

    updated menu.html fonts

--- webpages/index.js
+++ webpages/index.js
@@ -1,3 +1,6 @@
+// TODO:
+//   - change all onclicks to add event listener
+
 // FUNCTION TO ALTER CLASSNAMES OF NAVBAR ELEMENTS FOR RESPONSIVENESS //
 function navFunction() {
   console.log("entered navFunction");
@@ -92,13 +95,9 @@ function modalOpened() {
   window.onclick = function(e) {
     if (e.target == modal) {
       window.location.href = "#close";
+      body.classList.remove("no-scroll");
     }
   };
-}
-
-function modalClosed() {
-  let body = document.body;
-  body.classList.remove("no-scroll");
 }
 
 // TODO:
@@ -111,24 +110,34 @@ function modalClosed() {
 function emailValidate(email) {
   // validate email
   let emailIn = document.getElementById("userEmail");
+
   if (isEmail(email) == true) {
-    // If good change background to green
-    emailIn.style.border = "4px solid green";
+    // If good change background to green, remove possible error message
+    emailIn.style.border = "2px solid rgb(119, 221, 119)";
+    emailIn.style.backgroundColor = "rgb(119, 221, 119)";
   } else if (emailIn.value == "") {
     emailIn.style.border = "2px solid rgb(192, 191, 191)";
+    emailIn.style.backgroundColor = "rgb(192, 191, 191)";
   } else {
-    emailIn.style.border = "4px solid red";
+    emailIn.style.border = "2px solid rgb(255, 105, 97)";
+    emailIn.style.backgroundColor = "rgb(255, 105, 97)";
   }
 }
 
 function nameValidate(name) {
   // Ensure names have a value, and don't contain numbers
-  let regex = /^[a-zA-Z ]{2,30}$/;
-  if (!regex.test(name)) {
+  let nameIn = document.getElementById("userName");
+
+  if (isName(name) == true) {
+    nameIn.style.border = "2px solid rgb(119, 221, 119)";
+    nameIn.style.backgroundColor = "rgb(119, 221, 119)";
+  } else if (nameIn.value == "") {
+    nameIn.style.border = "2px solid rgb(192, 191, 191)";
+    nameIn.style.backgroundColor = "rgb(192, 191, 191)";
+  } else {
     // Name does not match the regex - contains numbers etc
-    alert("invalid name");
-    let nameIn = document.getElementById("userName");
-    nameIn.value = "";
+    nameIn.style.border = "2px solid rgb(255, 105, 97)";
+    nameIn.style.backgroundColor = "rgb(255, 105, 97)";
   }
 }
 
@@ -140,3 +149,46 @@ function isEmail(email) {
     return true;
   }
 }
+
+function isName(name) {
+  let regex = /^[a-zA-Z ]{2,30}$/;
+  if (!regex.test(name)) {
+    return false;
+  } else {
+    return true;
+  }
+}
+
+function sendBtnClicked() {
+  alert("clicked");
+  // First, if things are empty, output an error message in #errorMsg
+  // Then, check content of subject is > 1, and message length > 1.
+  // Then, check the RECAPTCHA has authorised the user.
+  // Save all 4 input fields into variables to be sent to a server.js function
+  let nameIn = document.getElementById("userName");
+  let emailIn = document.getElementById("userEmail");
+  let subjectIn = document.getElementById("mailSubject");
+  let messageIn = document.getElementById("mailMessage");
+
+  console.log(nameIn.value, emailIn.value, subjectIn.value, messageIn.value);
+
+  if (isName(nameIn.value) == "true") {
+    console.log("name is good");
+    if (isEmail(emailIn.value) == "true") {
+      console.log("email is good");
+      if (subjectIn.value.length > 1) {
+        console.log("subject is good");
+        if (messageIn.value.length > 1) {
+          console.log("message is good");
+          alert("all test passed");
+        }
+      }
+    }
+  }
+}
+
+// window.addEventListener("load", function() {
+//   document
+//     .getElementById("sendMessage")
+//     .addEventListener("click", sendBtnClicked);
+// });
