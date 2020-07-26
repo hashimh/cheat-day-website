3d226bd99b4f194966d1db98e48a646db86efe1c
Hashim Hussain <up815459@myport.ac.uk>
2020-07-26 18:38:47 +0100

    removed old scripts

--- webpages/index.js
+++ webpages/index.js
@@ -9,30 +9,6 @@ function navFunction() {
   }
 }
 
-// FUNCTION TO ALTER CLASSNAMES OF SIDENAV ELEMENTS FOR RESPONSIVENESS //
-document.addEventListener("DOMContentLoaded", function () {
-  let links = document.getElementsByClassName("side_a");
-
-  // Add event listener for clicking links
-  for (let i = 0; i < links.length; i++) {
-    links[i].addEventListener("click", function () {
-      let div = document.getElementsByName(this.innerHTML);
-      div[0].scrollIntoView({ behavior: "smooth" });
-    });
-  }
-
-  // Add event listener for scrolling to "links"
-  let divs = document.getElementsByClassName("menuitem");
-  window.addEventListener("scroll", function () {
-    let index = divs.length;
-    while (--index && window.scrollY + 100 < divs[index].offsetTop) {}
-    for (let i = 0; i < links.length; i++) {
-      links[i].classList.remove("active_a");
-      links[index].classList.add("active_a");
-    }
-  });
-});
-
 // FUNCTION TO RESET PAGE LOCATION ON REFRESH
 window.onbeforeunload = function () {
   window.scrollTo(0, 0);
